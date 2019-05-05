91a8c3ace0fb4b08627c8cf43c64b10080b6230e
pranavpks <pranavpks@hotmail.com>
2019-05-04 19:04:18 -0600

    with split model speed hack

---
+++ session_worker.py
@@ -0,0 +1,73 @@
+
+import tensorflow as tf
+import threading
+import Queue
+import time
+
+
+class SessionWorker():
+# from https://github.com/naisy/realtime_object_detection/blob/master/lib/session_worker.py
+
+    def __init__(self,tag,graph,config):
+        self.lock = threading.Lock()
+        self.sess_queue = Queue.Queue()
+        self.result_queue = Queue.Queue()
+        self.tag = tag
+        t = threading.Thread(target=self.execution,args=(graph,config))
+        t.setDaemon(True)
+        t.start()
+        return
+
+    def execution(self,graph,config):
+        self.is_thread_running = True
+        try:
+            with tf.Session(graph=graph,config=config) as sess:
+                while self.is_thread_running:
+                        while not self.sess_queue.empty():
+                            q = self.sess_queue.get(block=False)
+                            opts = q["opts"]
+                            feeds= q["feeds"]
+                            extras= q["extras"]
+                            if feeds is None:
+                                results = sess.run(opts)
+                            else:
+                                results = sess.run(opts,feed_dict=feeds)
+                            self.result_queue.put({"results":results,"extras":extras})
+                            self.sess_queue.task_done()
+                        time.sleep(0.005)
+        except:
+            import traceback
+            traceback.print_exc()
+        self.stop()
+        return
+
+    def is_sess_empty(self):
+        if self.sess_queue.empty():
+            return True
+        else:
+            return False
+
+    def put_sess_queue(self,opts,feeds=None,extras=None):
+        self.sess_queue.put({"opts":opts,"feeds":feeds,"extras":extras})
+        return
+
+    def is_result_empty(self):
+        if self.result_queue.empty():
+            return True
+        else:
+            return False
+
+    def get_result_queue(self):
+        result = None
+        if not self.result_queue.empty():
+            result = self.result_queue.get(block=False)
+            self.result_queue.task_done()
+        return result
+
+    def stop(self):
+        self.is_thread_running=False
+        with self.lock:
+            while not self.sess_queue.empty():
+                q = self.sess_queue.get(block=False)
+                self.sess_queue.task_done()
+        return
